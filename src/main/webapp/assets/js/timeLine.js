c6e5a4b325d4c5d9e9de077c63e50d13922e2fe0
QQY <candy_dr_qqy@hotmail.com>
2015-08-24 18:47:58 +0800

    timeLine modify

---
+++ src/main/webapp/assets/js/timeLine.js
@@ -0,0 +1,10 @@
+/**
+ * Created by qqy on 15/8/24.
+ */
+$.get(commonUrl + 'order/listUserOrder/1', function (data) {
+    var listTemple = Handlebars.compile($('#timeLine').html());
+    $('#tl-main').html(listTemple(data.ret_values.orders));
+    var listTemple = Handlebars.compile($('#title').html());
+    $('#tl-title').html(listTemple(data.ret_values));
+    console.log(data.ret_values);
+})
