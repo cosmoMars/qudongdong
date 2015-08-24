fc669b969d3ce400af2c1e00a78d5a8668669fa2
刘泽龙 <liuzelong@wondersgroup.com>
2015-08-24 17:45:44 +0800

    handler列表数据插入提交

---
+++ src/main/webapp/assets/js/common.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wade on 15/8/24.
+ */
+var commonUrl = 'http://101.231.124.8:45698/qdd/';
---
+++ src/main/webapp/assets/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wade on 15/8/24.
+ */
+$.get(commonUrl + 'order/listSportOrder', function (data) {
+    var listTemple = Handlebars.compile($('#listBox').html());
+    //$('#main-list').html(listTemple(data.ret_values));
+})
