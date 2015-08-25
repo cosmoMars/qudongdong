1e3453289214d59327d7ecef01beed898f893c67
QQY <candy_dr_qqy@hotmail.com>
2015-08-25 20:52:57 +0800

    页面跳转

--- src/main/webapp/assets/js/datingRequest.js
+++ src/main/webapp/assets/js/datingRequest.js
@@ -1,7 +1,33 @@
 /**
  * Created by 倩钰 on 2015/8/24.
  */
-$.get(commonUrl + 'orderCustomer/listOrderCustomer/1', function (data) {
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return (r[2]);
+    return null;
+}
+
+var userId = GetQueryString("userId");
+if (userId != null) {
+    var userId_ = decodeURIComponent(userId);
+}
+$.get(commonUrl + 'orderCustomer/listOrderCustomer/' + userId_, function (data) {
     var listTemple = Handlebars.compile($('#request').html());
     $('#dr-main').html(listTemple(data.ret_values));
-})
+});
+
+function responseCustomer() {
+    var customerId = document.getElementById("customerId").value;
+    console.log(customerId);
+    var responseCustomerUrl = commonUrl + 'orderCustomer/responseCustomer/' + customerId + '/' + 'agree';
+    $.get(responseCustomerUrl, function (data) {
+        if (data.ret_code == 0) {
+            $('#my-alert').modal({relatedTarget: this,})
+        }
+        else {
+            $('#status').html("请求失败！");
+            $('#my-alert').modal({relatedTarget: this,})
+        }
+    });
+}
--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -12,14 +12,24 @@ function GetQueryString(name) {
 var userId = GetQueryString("userId");
 if (userId != null) {
     var userId_ = decodeURIComponent(userId);
-    $('#imgUrl').attr('href','addDatings.html?userId='+userId_);
+    $('#imgUrl').attr('href', 'addDatings.html?userId=' + userId_);
 }
 
-$.get(commonUrl + 'order/listSportOrder/'+userId_, function (data) {
+$.get(commonUrl + 'order/listSportOrder/' + userId_, function (data) {
     var listTemple = Handlebars.compile($('#listBox').html());
     $('#main-list').html(listTemple(data.ret_values));
-    if(data.ret_values.sex=="Female"){
-
-
-    }
 })
+
+function generateOrderCustomer() {
+    var orderId = document.getElementById("orderId").value;
+    var generateOrderCustomerUrl = commonUrl + 'orderCustomer/generateOrderCustomer/ ' + orderId + '/' + userId_;
+    $.get(generateOrderCustomerUrl, function (data) {
+        if (data.ret_code == 0) {
+            $('#my-alert').modal({relatedTarget: this,})
+        }
+        else {
+            $('#status').html("加入失败！");
+            $('#my-alert').modal({relatedTarget: this,})
+        }
+    });
+}
