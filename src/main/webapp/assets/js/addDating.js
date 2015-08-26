02ab08f8a59f019117ee08f2940d002fd05f2660
刘泽龙 <liuzelong@wondersgroup.com>
2015-08-26 15:11:13 +0800

    增加日期

--- src/main/webapp/assets/js/addDating.js
+++ src/main/webapp/assets/js/addDating.js
@@ -26,25 +26,11 @@ $(function () {
     });
 
     $('#startTime-btn').on('click', function () {
-        $('#ad-startTime').modal({
-            relatedTarget: this,
-            onConfirm: function (e) {
-                $('#startTime-text').html(e.data);
-            },
-            onCancel: function (e) {
-            }
-        });
+        openOneSlot('start');
     });
 
     $('#endTime-btn').on('click', function () {
-        $('#ad-endTime').modal({
-            relatedTarget: this,
-            onConfirm: function (e) {
-                $('#endTime-text').html(e.data);
-            },
-            onCancel: function (e) {
-            }
-        });
+        openOneSlot('end');
     });
 
     $('#partner-btn').on('click', function () {
@@ -120,16 +106,21 @@ function generateOrder() {
     var location = $('#location-text').html();
     var partner = getPartner($('#partner-text').html());
     var age = getAge($('#age-text').html());
-    var startTime = $('#startTime-text').html();
-    var endTime = $('#endTime-text').html();
+    var startTime = $('#startTime-text').attr('data-time');
+    var endTime = $('#endTime-text').attr('data-time');
+    if(new Date(startTime)>= new Date(endTime)){
+        $('#status').html("开始时间不能大于结束时间哦！");
+        $('#my-alert').modal({relatedTarget: this});
+        return false;
+    }
     var content = $('#content-text').val();
     if (location.length == 0 || partner.length == 0 || age.length == 0 || startTime.length == 0 || endTime.length == 0 || content.length == 0) {
         $('#status').html("您还有字段未填写哦！");
-        $('#my-alert').modal({relatedTarget: this,})
+        $('#my-alert').modal({relatedTarget: this,});
     }
     else if (content.length >= 12) {
         $('#status').html("说明仅限12个字哦！");
-        $('#my-alert').modal({relatedTarget: this,})
+        $('#my-alert').modal({relatedTarget: this,});
     }
     else {
 
@@ -160,7 +151,7 @@ function generateOrder() {
             success: function (response) {
                 if (response.ret_code == 0) {
                     $('#status').html(response.ret_values);
-                    $('#my-alert').modal({relatedTarget: this,})
+                    $('#success-alert').modal({relatedTarget: this,})
                 }
                 else {
                     $('#status').html(response.ret_values);
@@ -173,4 +164,90 @@ function generateOrder() {
             }
         });
     }
+}
+
+function toMain() {
+    location.href = 'main.html?userId='+userId_;
+}
+
+function startDone() {
+    var results = SpinningWheel.getSelectedValues();
+    var startDate = new Date(), month = startDate.getMonth() + 1,
+        startDateString = startDate.getFullYear() + '-' + month + '-' + startDate.getDate() + ' ' + results.keys.join(':');
+    var $startTime = $('#startTime-text');
+    $startTime.html(results.values.join(''));
+    $startTime.attr('data-time', startDateString);
+//        document.getElementById('result').innerHTML = 'values: ' + results.values.join(' ') + '<br />keys: ' + results.keys.join(', ');
+}
+function endDone() {
+    var results = SpinningWheel.getSelectedValues();
+    var endDate = new Date(), month = endDate.getMonth() + 1,
+        endDateString = endDate.getFullYear() + '-' + month + '-' + endDate.getDate() + ' ' + results.keys.join(':');
+    //console.log(endDateString);
+    var $endTime = $('#endTime-text');
+    $endTime.html(results.values.join(''));
+    $endTime.attr('data-time', endDateString);
+//        document.getElementById('result').innerHTML = 'values: ' + results.values.join(' ') + '<br />keys: ' + results.keys.join(', ');
+}
+
+function cancel() {
+//        document.getElementById('result').innerHTML = 'cancelled!';
+}
+
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        window.scrollTo(0, 0);
+    }, 100);
+}, true);
+
+function openOneSlot(type) {
+    var hours = {
+        0: '00点',
+        1: '01点',
+        2: '02点',
+        3: '03点',
+        4: '04点',
+        5: '05点',
+        6: '06点',
+        7: '07点',
+        8: '08点',
+        9: '09点',
+        10: '10点',
+        11: '11点',
+        12: '12点',
+        13: '13点',
+        14: '14点',
+        15: '15点',
+        16: '16点',
+        17: '17点',
+        18: '18点',
+        19: '19点',
+        20: '20点',
+        21: '21点',
+        22: '22点',
+        23: '23点'
+    };
+    var mins = {
+        0: '00分',
+        5: '05分',
+        10: '10分',
+        15: '15分',
+        20: '20分',
+        25: '25分',
+        30: '30分',
+        35: '35分',
+        40: '40分',
+        45: '45分',
+        50: '50分',
+        55: '55分',
+    };
+    SpinningWheel.addSlot(hours, 'center', 11);
+    SpinningWheel.addSlot(mins, 'center', 30);
+    SpinningWheel.setCancelAction(cancel);
+    if (type === 'start')
+        SpinningWheel.setDoneAction(startDone);
+    else
+        SpinningWheel.setDoneAction(endDone);
+    SpinningWheel.open();
 }
