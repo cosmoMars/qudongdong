1cb82190c292f7c1813e064d2b69b14914d2b50d
QQY <candy_dr_qqy@hotmail.com>
2015-08-28 16:16:57 +0800

    selectSports

---
+++ src/main/webapp/assets/js/selectSports.js
@@ -0,0 +1,13 @@
+/**
+ * Created by qqy on 15/8/28.
+ */
+
+var flag;
+function selectSports(){
+    if(flag){
+        obj.className="boldStyle";
+    } else {
+        obj.className="";
+    }
+    flag = !flag;
+}
