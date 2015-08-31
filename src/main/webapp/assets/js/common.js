18862edb53a21539d28f52137b0f7b0b3ef39b86
QQY <candy_dr_qqy@hotmail.com>
2015-08-31 17:40:43 +0800

    ss modify

--- src/main/webapp/assets/js/common.js
+++ src/main/webapp/assets/js/common.js
@@ -1,8 +1,8 @@
 /**
  * Created by wade on 15/8/24.
  */
-var commonUrl = 'http://101.231.124.8:45698/qdd/';
-//var commonUrl = 'http://localhost:8080/';
+//var commonUrl = 'http://101.231.124.8:45698/qdd/';
+var commonUrl = 'http://localhost:8080/';
 function getSex(type) {
     if (type == "Male") {
         return "男";
